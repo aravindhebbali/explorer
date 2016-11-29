89ea87168798950066628d84997871fc382b1a60
rsquaredin <rsquaredin@gmail.com>
2016-11-29 21:54:06 +0530

    Test binom_prob function

--- logic/logic_binom.R
+++ logic/logic_binom.R
@@ -1,3 +1,7 @@
 output$binom_shape <- renderPlot({
   binom_plot(input$binom_n, input$binom_p)
 })
+
+output$bprob_plot <- renderPlot({
+  binom_prob(input$bprob_n, input$bprob_p, input$bprob_s, input$bprob_tail)
+})
--- ui/ui_binom.R
+++ ui/ui_binom.R
@@ -20,6 +20,30 @@ tabPanel('Binomial Distribution', value = 'tab_binom',
             column(8,
               plotOutput('binom_shape', height = '400px')
             )
+          ),
+          tabPanel('Find Probability',
+            column(4,
+              column(6, align = 'center',
+                br(),
+                br(),
+                numericInput('bprob_n', 'Number of trials', value = 10, min = 1,
+                  step = 1
+                ),
+                numericInput('bprob_p', 'Probability', value = 0.3, min = 0,
+                  max = 1, step = 0.01
+                ),
+                numericInput('bprob_s', 'Success', value = 1, min = 0,
+                step = 1
+                ),
+                selectInput('bprob_tail', 'Tail',
+                  choices = c('lower', 'upper', 'exact', 'interval'),
+                  selected = 'lower'
+                )
+              )
+            ),
+            column(8,
+              plotOutput('bprob_plot', height = '400px')
+            )
           )
         )
       )
