f87e6f8f8fd069dd4c8633ec45b299525be07490
rsquaredin <rsquaredin@gmail.com>
2016-12-18 14:03:53 +0530

    modify chisquare plot output

--- logic/logic_chisq.R
+++ logic/logic_chisq.R
@@ -1,9 +1,9 @@
 output$chisq_shape <- renderPlot({
-  chisq_plot(input$chisq_df, input$chisq_norm)
+  chi_plot(input$chisq_df, input$chisq_norm)
 })
 
 output$chiprob_plot <- renderPlot({
-  binom_prob(input$chiprob_p, input$chiprob_df, input$chiprob_tail)
+  chi_prob(input$chiprob_p, input$chiprob_df, input$chiprob_tail)
 })
 
 output$chiperc_plot <- renderPlot({
